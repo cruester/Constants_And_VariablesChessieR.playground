45001a9b7ba99f47d184b96372fb38ef28a8ee20
cruester <chessie.ruester@sbcglobal.net>
2015-09-01 17:28:03 -0500

    init commit
    experimenting with constants and variables

---
+++ Contents.swift
@@ -0,0 +1,59 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//int variable
+var age = 20
+
+
+//float variable
+var height = 57.5
+
+
+//double variable
+var pay = 7.25
+
+
+//int constant
+let daysInWeek = 7
+
+
+//float constant
+let gpa = 3.8
+
+
+//double constant
+let pi = 3.14
+
+
+//explicit string constant
+let name : String = "Paula"
+
+//explicit float constant
+let radius : Float = 1.5
+
+//explicit double constant
+let hoursToRead : Double = 2.25
+
+//initialize constant and then try to change it
+let numHours = 4
+
+//numHours = 12
+
+//explicit float constant 
+let numCandyBars : Float = 4
+
+numCandyBars
+
+/* Evidently Xcode does not care. Makes sense because this could just as
+easily be 4.0000 */
+
+
+
+
+
+
+
+
+
+
